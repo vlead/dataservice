7acff4c2d5b4977bec110918cf4e6965df73e41c
Ambika <ambika@vlabs.ac.in>
2014-12-05 09:51:07 +0530

    Corrected the names of functions in test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,18 +8,30 @@ import json
 class MyTest(unittest.TestCase):
     def setUp(self):
         self.proc = subprocess.Popen(["nohup", "python", "app.py"])
+  
     def tearDown(self):
         self.proc.terminate()
-    def testLabId(self):
+  
+    def test_Lab_Id(self):
         r = requests.get('http://localhost:8080/labs/547d5cae3e56404101173170').json()
         print (json.dumps(r))
         expected_data = '{"status": "Hosted", "lab_id": "cse01", "hosted_url": "http://virtual-labs.ac.in/labs/cse01/", "auto_hostable": "Yes", "type_of_lab": "", "simulation": "Yes", "content": "Yes", "lab_name": "Data Structures", "sources_available": "Yes", "repo_url": "https://bitbucket.org/virtual-labs/cse01-ds_new", "discipline_name": "cse", "remarks": "Completed ", "integration_level": 5, "id": "547d5cae3e56404101173170"}'
         self.assertEqual(json.dumps(r),expected_data)
-    def testDiscipline(self):
+  
+    def test_Discipline(self):
         r = requests.get('http://localhost:8080/labs/discipline/ece').json()
         print (json.dumps(r))
         expected_data = '[{"auto_hostable": "Yes", "lab_id": "cse02", "_id": {"$oid": "547d5cae3e56404101173171"}, "hosted_url": "http://virtual-labs.ac.in/labs/cse02/", "number_of_experiments": 10, "type_of_lab": "", "institute_id": 4, "simulation": "Yes", "content": "Yes", "lab_name": "Computer Programming", "lab_deployed": "Yes", "sources_available": "Yes", "status": "Hosted", "repo_url": "https://bitbucket.org/virtual-labs/cse02-programming", "discipline_name": "ece", "remarks": "Completed ", "developer": "jawahar@iiit.ac.in", "integration_level": 5, "id": 2, "web2.0_compliance": "No"}]'
         self.assertEqual(json.dumps(r), expected_data)
+    
+    def test_Lab_Id_With_Param(self):
+        
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
