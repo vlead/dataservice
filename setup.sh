21629ba884fa172b3bb8647666b1eea37cdd74cd
Anon Ray <rayanon004@gmail.com>
2015-08-18 09:17:59 +0530

    Update setup.sh to execute using both sudo and normal user
      - Use sudo to install deb package dependencies
      - Don't use sudo to run python setup.py install to install dataservice as a
        normal user and not super user.

--- setup.sh
+++ setup.sh
@@ -2,16 +2,17 @@
 # Shell script to install deb package dependencies as well as python package
 # dependencies for dataservice.
 
-if [[ `id -u` -ne 0 ]]; then
-  echo "You have to execute this script as super user!"
-  exit 1;
-fi
 # Update the packages
 echo "Installing pre-requisite dependencies.."
-apt-get update
-apt-get install mysql-server python-dev libmysqld-dev
+echo "To install system-wide packages, you have to enter sudo password.."
+sudo apt-get update
+sudo apt-get install mysql-server python-dev libmysqld-dev
 echo "Installing dependencies.."
 # Installing dependencies
+python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0
+if [[ $INVENV -eq 0 ]]; then
+  source venv/bin/activate
+fi
 python setup.py install
 if [[ $? -ne 0 ]]; then
   echo "Installation failed!"
