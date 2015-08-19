9edc8cc2ff691e72a946341d07168731d1feb991
Ambika <ambika@vlabs.ac.in>
2015-08-19 12:55:02 +0530

    Made following changes in two files:
     + In src/db.py :
      - Added a class for Institute Name type.
      - Added decorators for typechecking in set_name(), set_pic() and set_iic()
        methods.
    
     + In tests/test_db.py :
      - Wrote test cases for Institute entity.

--- tests/test_db.py
+++ tests/test_db.py
@@ -12,7 +12,7 @@ class DBTest(TestCase):
 
     TESTING = True
     config = {
-        'SQLALCHEMY_DATABASE_URI': ''
+        'SQLALCHEMY_DATABASE_URI': 'mysql+oursql://root:root@localhost:8080/vlabs_database'
     }
 
     def create_app(self):
@@ -44,7 +44,61 @@ class DBTest(TestCase):
         self.assertEqual(new_name.value, "John")
         self.assertRaises(TypeError, Name, "123dasd")
 
+    # Tests for Institute entity.
+    def test_set_institute_pic(self):
+        print "test_set_institute_pic()"
+        instt = Institute(name="IndianInstitute", PIC="Avinash", IIC="Amit")
+        new_pic = Name("John Doe")
+        print new_pic
+        instt.set_pic(new_pic)
+        self.assertEqual(instt.PIC.value, "John Doe")
 
+    def test_set_institute_iic(self):
+        print "test_set_institute_iic()"
+        instt = Institute(name="IndianInstitute", PIC="John", IIC="Amit")
+        new_iic = Name("Jane Doe")
+        print new_iic
+        instt.set_iic(new_iic)
+        self.assertEqual(instt.IIC.value, "Jane Doe")
 
+    def test_set_institute_name(self):
+        print "test_set_institute_name()"
+        instt = Institute(name="IndianInstitute", PIC="John", IIC="Amit")
+        new_name = InstituteName("IIT-Hyd Telangana")
+        instt.set_name(new_name)
+        self.assertEqual(instt.name.value, "IIT-Hyd Telangana")
+        
+    def test_get_institute_pic(self):
+        print "test_get_institute_pic()"
+        instt = Institute(name="IndianInstitute", PIC="John", IIC="Jane")
+        inst_pic = instt.get_pic()
+        self.assertEqual(inst_pic, "John")
+
+    def test_get_institute_iic(self):
+        print "test_get_institute_iic()"
+        instt = Institute(name="IndianInstitute", PIC="John", IIC="Jane")
+        inst_iic = instt.get_iic()
+        self.assertEqual(inst_iic, "Jane")
+
+    def test_get_id_of_institute(self):
+        print "test_get_id_of_institute()"
+        instt = Institute(id="1", name="SomeInstitute")
+        institute_id = instt.get_id()
+        self.assertEqual(institute_id, "1")
+
+    def test_get_institute_by_id(self):
+        print "test_get_institute_by_id()"
+        instt = Institute(name="IndianInstitute", PIC="John", IIC="Jane")
+        instt.save()
+        self.assertEqual(instt.get_institute_by_id(1).name, "IndianInstitute")
+
+  #  def test_get_institute_by_developer(self):
+  #     print "test_get_institute_by_developer()"
+  #     instt = Institute(name="IITHyd")
+  #     instt.save()
+  #     dev = Developer(name="John Doe", institute_id=instt.id)
+  #     self.assertEqual(instt.get_institute_by_developer(dev).name, "IITHyd#")
+
+       
 if __name__ == '__main__':
     unittest.main()
