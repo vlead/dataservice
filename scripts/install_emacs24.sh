975be2b88f15cf68e65c0409f8af64af481fb810
Anon Ray <rayanon004@gmail.com>
2015-10-08 17:32:31 +0530

    Add script to install Emacs 24

---
+++ scripts/install_emacs24.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ `id -u` -ne 0 ]]; then
+  echo "You have to execute this script as super user!"
+  exit 1;
+fi
+
+apt-get install build-essential
+apt-get build-dep emacs24
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
+tar -xzvf emacs-24.4.tar.gz
+rm emacs-24.4.tar.gz
+cd emacs-24.4
+./configure --prefix=/opt/emacs
+make
+sudo make install
+export PATH=/opt/emacs/bin:$PATH
+echo "export PATH=/opt/emacs/bin:\$PATH" >> ~/.bashrc
+mkdir -p ~/emacs/lisp
+cd emacs/lisp
+wget http://orgmode.org/org-8.2.10.tar.gz
+tar zxvf org-8.2.10.tar.gz
